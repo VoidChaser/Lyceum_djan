599398dce07ecd6960607f7b3415c08550ff0bb2
trofi <trofi.troll@yandex.ru>
2024-02-05 17:29:29 +0300

    Secret key replaced to fake

--- lyceum/settings.py
+++ lyceum/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from secrett.key import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+SECRET_KEY = 'fake_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
